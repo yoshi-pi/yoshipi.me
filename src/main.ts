371de3739ac3699c8983f592d7b0d0e113e6836d
Yoshipi <yoshipyft@gmail.com>
2024-12-07 15:52:25 +0900

    Viteの導入

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
+import './style.css';
+
 window.addEventListener('mousemove', (event) => {
-  document.querySelector(
+  document.querySelector<HTMLElement>(
     '.profile-image-container'
-  ).style.background = `linear-gradient(${event.clientY}deg, #34d43a, #f9ca24)`;
+  )!.style.background = `linear-gradient(${event.clientY}deg, #34d43a, #f9ca24)`;
 });
-document.querySelector('.year').textContent = new Date().getFullYear();
+document.querySelector('.year')!.textContent = String(new Date().getFullYear());
 const paths = ['path-logo1', 'path-logo2', 'path-logo3', 'path-logo4'];
 for (const path of paths) {
-  const pathEl = document.querySelector(`.${path}`);
+  const pathEl = document.querySelector(`.${path}`) as SVGPathElement;
 
   requestAnimationFrame(() => {
-    pathEl.style.strokeDashoffset = 0;
+    pathEl.style.strokeDashoffset = '0';
     function HideAndShowLogo() {
       setTimeout(() => {
         pathEl.classList.add(`${path}-reverse`);
-        pathEl.style.strokeDashoffset = 1;
+        pathEl.style.strokeDashoffset = '1';
         setTimeout(() => {
           pathEl.classList.remove(`${path}-reverse`);
-          pathEl.style.strokeDashoffset = 0;
+          pathEl.style.strokeDashoffset = '0';
           HideAndShowLogo();
         }, 1400 + 1000); // 消える時間+消えてから表示する時間
       }, 3200 + 10000); // 表示する時間+次の消えるまでの時間
