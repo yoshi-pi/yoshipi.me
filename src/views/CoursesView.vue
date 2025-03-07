bc2d1cacd24a8f9c682975e7767c4ec0c8473f7f
Yoshipi <yoshipyft@gmail.com>
2025-03-08 00:43:46 +0900

    アニメーションのバグの調査

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -30,40 +30,38 @@ function loaded() {
   <main>
     <h2>{{ courses.title }}</h2>
     <div v-if="!isLoaded" style="height: 800px"></div>
-    <Transition name="fade">
-      <div v-show="isLoaded" class="courses">
-        <div v-for="course in courses.list" :key="course.id" class="course-container">
-          <a :href="course.link">
-            <div class="image-container">
-              <img
-                :srcset="`${course.image.url}?fit=crop&w=360&h=202&fm=webp&dpr=1 1x,
+    <div v-show="isLoaded" class="courses">
+      <div v-for="course in courses.list" :key="course.id" class="course-container">
+        <a :href="course.link">
+          <div class="image-container">
+            <img
+              :srcset="`${course.image.url}?fit=crop&w=360&h=202&fm=webp&dpr=1 1x,
                 ${course.image.url}?fit=crop&w=360&h=202&fm=webp&dpr=2 2x,
                 ${course.image.url}?fit=crop&w=360&h=202&fm=webp&dpr=3 3x`"
-                :src="`${course.image.url}?fit=crop&w=360&h=202&fm=webp`"
-                :alt="course.title"
-                @load="loaded"
-              />
-            </div>
-            <div
-              style="
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                margin-top: 8px;
-              "
-            >
-              <p>{{ course.title }}</p>
-              <p style="margin-right: 8px; opacity: 0.8; font-size: 14px">
-                {{ course.durationHour }}
-              </p>
-            </div>
-            <p style="font-size: 14px; margin-top: 8px; text-decoration: none">
-              {{ course.description }}
+              :src="`${course.image.url}?fit=crop&w=360&h=202&fm=webp`"
+              :alt="course.title"
+              @load="loaded"
+            />
+          </div>
+          <div
+            style="
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              margin-top: 8px;
+            "
+          >
+            <p>{{ course.title }}</p>
+            <p style="margin-right: 8px; opacity: 0.8; font-size: 14px">
+              {{ course.durationHour }}
             </p>
-          </a>
-        </div>
+          </div>
+          <p style="font-size: 14px; margin-top: 8px; text-decoration: none">
+            {{ course.description }}
+          </p>
+        </a>
       </div>
-    </Transition>
+    </div>
   </main>
 </template>
 
